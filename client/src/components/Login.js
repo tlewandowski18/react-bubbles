7505e98b2347004159b9f8b628e3605301e66098
tlewandowski18 <tlewandowski18@gmail.com>
2020-03-22 21:48:05 -0400

    complete all MVP requirements

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [creds, setCreds] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleChange = e => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(creds)
+    axios.post('http://localhost:5000/api/login', creds)
+      .then(res => {
+        window.localStorage.setItem("token", res.data.payload)
+        props.history.push('/bubbles')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <input
+          placeholder="Username"
+          name="username"
+          value={creds.username}
+          onChange={handleChange}
+          className="login-input"
+        />
+        <input
+          placeholder="Password"
+          name="password"
+          value={creds.password}
+          onChange={handleChange}
+          className="login-input"
+        />
+        <button type="submit" className="login-button">Login</button>
+      </form>
     </>
   );
 };
